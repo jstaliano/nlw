b5c4523cbc131231e0cf3c19cc03920fd325a69d
jstaliano <jstaliano@gmail.com>
2020-06-04 07:24:29 -0300

    Terceiro dia Complemento aula 2

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -28,22 +28,23 @@
         const stateInput = document.querySelector("[name=state]")
         //const ufValue = event.target.value
         //console.log(ufValue)
+        
         const ufValue = event.target.value
         const indexOfSelectedState = event.target.selectedIndex
         stateInput.value = event.target.options[indexOfSelectedState].text
         
         const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-        
-        
-        fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios")
+        citySelect.innerHTML=""
+        citySelect.disabled = true
+        fetch(url)
         .then( res => res.json() )
         .then( cities => {
             
             for(const city of cities){                
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option> `
+                 citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option> `
                 
             }
-            citySelect.disabled = false
+            citySelect.disabled= false
         } )
         
     }
@@ -51,6 +52,49 @@
     document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+    // itens de coleta
+    //addEventListener    
+    const itemsToCollect = document.querySelectorAll(".items-grid li")
+    for (const item of itemsToCollect){
+        item.addEventListener("click",handleSelectedItem)
+    }
+    const collectedItems = document.querySelector("input[name=items]")
+    let selectedItems= []
+    function handleSelectedItem(event){
+        const itemLi = event.target
+        itemLi.classList.toggle("selected")
+        const itemId = itemLi.dataset.id
+        //console.log(event.target.dataset.id)
+        
+        //verificar se clicou em algum item que não está no array
+        // se sim -> adicionar no array 
+        const alreadySelected = selectedItems.findIndex( item => {
+            const itemFound = item == itemId
+            return itemFound
+        })
+        //***  reescrevendo função acima de forma contraída
+        //  const alreadySelected = selectedItems.findIndex( item => item == itemId)
+        //console.log(alreadySelected)
+        // se já estiver selecionado -> retirar do array
+        if (alreadySelected >= 0){
+            //removendo item do array
+            const filteredItems = selectedItems.filter( item=> {
+                const itemsDifferent = item != itemId
+                return itemsDifferent
+            })
+            //console.log(filteredItems)
+            selectedItems = filteredItems
+        }else {
+            // adicionar array
+            selectedItems.push(itemId)
+        }
+        // atualizar o campo
+        collectedItems.value = selectedItems
+        
+    }
+        
 
 
     
